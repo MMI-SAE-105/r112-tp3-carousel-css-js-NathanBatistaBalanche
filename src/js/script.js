351075985a8832f2cbd78ee8bfc4c716628b52a2
NathanBatistaBalanche <nathan.batista_balanche@edu.univ-fcomte.fr>
2024-11-20 11:21:34 +0100

    Défilement en fonction de la largeur d'une image 2

--- src/js/script.js
+++ src/js/script.js
@@ -4,20 +4,22 @@
 const carousel = document.querySelector(".carousel__container");
 const prevButton = document.querySelector(".carousel__button--prev");
 const nextButton = document.querySelector(".carousel__button--next")
+const premierItem = document.querySelector(".carousel__item");
+
 
 // Largeur de défilement d’un item
 if (carousel) {
     // Scroll au clic sur le bouton précédent
     prevButton.addEventListener("click", () => {
-        carousel.scrollBy({ left: -260, behavior: "smooth" });;
+        const scrollAmount = premierItem.clientWidth;
+        carousel.scrollBy({ left: -scrollAmount, behavior: "smooth" });;
     });
   
     // Scroll au clic sur le bouton suivant
     nextButton.addEventListener("click", () => {
-        carousel.scrollBy({ left : 260, behavior: "smooth"});
+        const scrollAmount = premierItem.clientWidth;
+        carousel.scrollBy({ left : scrollAmount, behavior: "smooth"});
     });
   }
 
-const premierItem = document.querySelector(".carousel__item");
 
-scrollAmount = premierItem.clientWidth;
